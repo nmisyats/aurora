919c7f460bf00df015e74c06a181e0df8a25e441
Nazar Misyats <nazar.misyats@gmail.com>
2025-06-12 22:54:40 +0900

    Renamed flux to flux_distrib

--- aurora/cli.py
+++ aurora/cli.py
@@ -198,7 +198,7 @@ def create_train_command_for_model(model_name: str, model_cls, config_cls):
         if plot:
             recon.eval_mode()
             recon_xy = xy_grid(bbox.xy_min, bbox.xy_max, plot_res_x, plot_res_y)
-            estimated_f = recon.flux(recon_xy).cpu()
+            estimated_f = recon.flux_distrib(recon_xy).cpu()
             rec_xy_min = bbox.xy_min.cpu()
             rec_xy_max = bbox.xy_max.cpu()
             if ref_flux_data is not None and ref_flux_config is not None:
@@ -391,7 +391,7 @@ def generate_reconstructed_flux(
     xy_min = recon.bbox.xy_min
     xy_max = recon.bbox.xy_max
     xy = xy_grid(xy_min, xy_max, res_x, res_y)
-    f = recon.flux(xy)
+    f = recon.flux_distrib(xy)
 
     if save is not None:
         data.save_3d_grid_data(f, save)
--- aurora/reconstruction.py
+++ aurora/reconstruction.py
@@ -39,9 +39,9 @@ class Reconstruction:
         else:
             self._training = False
 
-    def flux(self, xy: torch.Tensor) -> torch.Tensor:
+    def flux_distrib(self, xy: torch.Tensor) -> torch.Tensor:
         """
-        Calculate the electron flux at points xy.
+        Calculate the electron flux distribution at points xy.
         Args:
             xy (torch.Tensor): Tensor of shape (..., 2) in South-East coordinates.
         
@@ -51,9 +51,9 @@ class Reconstruction:
         """
         if not self._training:
             with torch.no_grad():
-                return self.flux_model.flux(xy)
+                return self.flux_model.flux_distrib(xy)
         else:
-            return self.flux_model.flux(xy)
+            return self.flux_model.flux_distrib(xy)
 
     def emis_rate(self, p_frame: torch.Tensor) -> torch.Tensor:
         """
@@ -67,7 +67,7 @@ class Reconstruction:
         """
         p_enu = self.frame.to_local_enu(p_frame, is_point=True)
         xy, z = p_frame[...,:2], p_enu[...,2]
-        f = self.flux(xy)
+        f = self.flux_distrib(xy)
         return phy.emis_rate(z, f, self.M_emis, self.z_edges)
     
     def elec_dens(self, p_frame: torch.Tensor) -> torch.Tensor:
@@ -82,7 +82,7 @@ class Reconstruction:
         """
         p_enu = self.frame.to_local_enu(p_frame, is_point=True)
         xy, z = p_frame[...,:2], p_enu[...,2]
-        f = self.flux(xy)
+        f = self.flux_distrib(xy)
         return phy.elec_dens(z, f, self.M_dens, self.z_edges)
 
     def int_emis_ray(
@@ -121,7 +121,7 @@ class Reconstruction:
             t_frame, p_frame = geom.create_ray_points(ro, rd, tn, tf, ray_bins, self.device)
             p_enu = self.frame.to_local_enu(p_frame, is_point=True)
             xy, z = p_frame[:,:2], p_enu[:,2]
-            f = self.flux(xy)
+            f = self.flux_distrib(xy)
             l = phy.emis_rate(z, f, self.M_emis, self.z_edges)
             g = phy.int_emis_rayleigh(t_frame, l)
             g = g # * self.frame.metric_scale(rd) not required?
