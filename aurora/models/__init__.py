950c9f4c6133f1b5d0bd4373a333cea0caf90d8e
Nazar Misyats <nazar.misyats@gmail.com>
2025-06-23 02:39:42 +0900

    Renamed basis_mlp.py into poly_mlp.py

--- aurora/models/__init__.py
+++ aurora/models/__init__.py
@@ -1,7 +1,7 @@
 from aurora.models.flux_model import FluxModel
 from aurora.models.grid_flux import GridSampledFlux
 from aurora.models.spectral_mlp import SpectralMLP
-from aurora.models.basis_mlp import PolyMLP
+from aurora.models.poly_mlp import PolyMLP
 from aurora.models.hybrid_mlp import HybridMLP
 
 from aurora.models.utils import save_model, load_model, load_grid_model
