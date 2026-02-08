4a5d65e397ac1d658d94d4356059f33b4ae4f894
Nazar Misyats <nazar.misyats@gmail.com>
2026-02-07 16:16:37 -0800

    Added wavelength filtering in load_cameras and docstring

--- aurora/data.py
+++ aurora/data.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 from dataclasses import dataclass
-from typing import Union, Optional, List, Dict, Tuple
+from typing import Union, Optional, List, Dict
 
-from schema import Schema, Optional as Option, And, Or, Use
+from schema import Schema, Optional as And, Or, Use
 import yaml
 try:
     from yaml import CLoader as Loader
@@ -34,6 +34,10 @@ def save_yaml(file_path: PathLike, data):
         yaml.dump(data, f, Dumper=Dumper)
 
 def resolve_relative_to_base(target_path: PathLike, base_path: PathLike) -> Path:
+    """
+    Return an absolute, resolved Path for `target_path`; if it is relative, interpret
+    it relative to `base_path`.
+    """
     target_path = Path(target_path)
     if target_path.is_absolute():
         return target_path.resolve()
@@ -43,6 +47,10 @@ def resolve_relative_to_base(target_path: PathLike, base_path: PathLike) -> Path
     return abs_target.resolve()
 
 def path_validator(base_path: Optional[PathLike] = None):
+    """
+    Creates schema validator that ensures a given path is a valid sub-path of the
+    provided base_path, if not absolute.
+    """
     validators = [Use(Path)]
     if base_path is not None:
         validators.append(Use(lambda p: resolve_relative_to_base(p, base_path)))
@@ -137,24 +145,50 @@ class CameraInfo:
     location_name: str
     wavelength: Optional[str] = None
 
-def load_cameras(cam_pos_set: PathLike, cam_images_dir: PathLike) -> List[Camera]:
-    cam_images_dir = Path(cam_images_dir)
-    if not cam_images_dir.exists():
-        raise FileNotFoundError(f"Camera image directory {cam_images_dir} not found.")
-    cams_info = load_camera_positions(cam_pos_set)
+def load_cameras(set_path: PathLike, imgs_dir: PathLike, wl_filter: Optional[List[str]] = None) -> List[Camera]:
+    """
+    Loads cameras from a position description file and image data directory.
+    
+    Args:
+        set_path: Path to camera position .set file.
+        imgs_dir: Path to the directory containing camera image data. The directory
+            structure is assumed to be either imgs_dir/{camera_location}/*.dat for
+            single wavelength or imgs_dir/{camera_location}/{wavelength}/*.dat for
+            multiple wavelengths
+        wl_filter (optional): List restricting the camera wavelengths to be loaded.
+            If None, all available camera data is loaded.
+    
+    Returns:
+        List of cameras.
+    
+    Raises:
+        FileNotFoundError: If the image directory or .set file path doesn't exist.
+    """
+    imgs_dir = Path(imgs_dir)
+    if not imgs_dir.exists():
+        raise FileNotFoundError(f"Camera image directory {imgs_dir} not found.")
+    
+    infos = load_camera_positions(set_path)
+    if wl_filter is not None:
+        infos = filter(lambda c: c.wavelength in wl_filter, infos)
+    
     cameras = []
-    for cam_info in cams_info:
-        cam_dir = cam_images_dir / cam_info.location_name
-        if cam_info.wavelength is not None:
-            cam_dir = cam_dir / cam_info.wavelength
+    for info in infos:
+        cam_dir = imgs_dir / info.location_name
+        if info.wavelength is not None:
+            cam_dir = cam_dir / info.wavelength
         if not cam_dir.exists():
-            print(f"Warning: couldn't find images for camera {cam_info.camera_id}.")
+            print(f"Warning: couldn't find images for camera {info.camera_id}.")
             continue
-        cam = load_camera(cam_info, cam_dir)
+        cam = load_camera(info, cam_dir)
         cameras.append(cam)
     return cameras
 
 def load_camera(cam_info: CameraInfo, cam_dir: PathLike):
+    """
+    Loads a single camera using the provided information in `cam_info` and loading
+    images `image.dat`, `az_cam.dat` and `ze_cam.dat` from `cam_dir`.
+    """
     cam_dir = Path(cam_dir)
     image = load_matrix_data(cam_dir / "image.dat")
     azimuth = load_matrix_data(cam_dir / "az_cam.dat")
@@ -175,13 +209,13 @@ def load_camera(cam_info: CameraInfo, cam_dir: PathLike):
 
 def load_camera_positions(set_path: PathLike) -> List[CameraInfo]:
     """
-    Load camera positions from a .set file.
+    Load camera positions and information from a .set file.
     
     Args:
         set_path (PathLike): Path to the .set file
         
     Returns:
-        List of CameraData objects with all available data
+        List of CameraInfo objects parsed from the .set file.
         
     Raises:
         ValueError: If a camera section is missing required data.
