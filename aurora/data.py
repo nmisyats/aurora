728ff4ad3d9d96839af652457bc08c686202afef
Nazar Misyats <nazar.misyats@gmail.com>
2026-02-12 22:36:06 -0800

    Added flux loading function

--- aurora/data.py
+++ aurora/data.py
@@ -310,6 +310,25 @@ def load_radar_point_cloud(dat_path: PathLike):
     return RadarPointCloud(lat, lon, h, d)
 
 
+def load_flux_data(
+        dat_path: PathLike,
+        nx: Optional[int] = None,
+        ny: Optional[int] = None,
+        nE: Optional[int] = None,
+        mul_by_pi: bool = False
+    ):
+    if nx and ny and nE:
+        flux = np.loadtxt(dat_path, dtype=np.float32)
+        flux = np.reshape(flux, (nE, nx, ny), order="F")
+        flux = flux.transpose(0, 2, 1) # (energy, southward, eastward)
+        flux = np.transpose(flux, (1, 2, 0)) # (southward, eastward, energy)
+        flux = torch.from_numpy(flux)
+    else:
+        flux = load_3d_grid_data(dat_path)
+    if mul_by_pi:
+        flux = flux * torch.pi  # [cm^-2 s^-1 eV^-1]
+    return flux
+
 def load_emission_matrix(dat_path: PathLike):
     return load_matrix_data(dat_path).T
 
@@ -321,6 +340,7 @@ def load_altitude_bins(dat_path: PathLike):
 
 def load_energy_bins(dat_path: PathLike):
     return load_matrix_data(dat_path).flatten()
+
 
 def load_matrix_data(dat_path: PathLike):
     mat = np.loadtxt(dat_path, dtype=np.float32)
--- aurora/models/utils.py
+++ aurora/models/utils.py
@@ -2,7 +2,7 @@ import torch
 
 from aurora.models.flux_model import FluxModel
 from aurora.models.grid_flux import GridSampledFlux
-from aurora.data import PathLike, load_config, load_3d_grid_data
+from aurora.data import PathLike, load_config, load_flux_data
 
 
 def save_model(model: FluxModel, path: PathLike):
@@ -13,6 +13,6 @@ def load_model(path: PathLike) -> FluxModel:
 
 def load_grid_model(flux_path: PathLike, config_path: PathLike):
     config = load_config(config_path)
-    data = load_3d_grid_data(flux_path)
+    data = load_flux_data(flux_path)
     grid_f = GridSampledFlux(config, data)
     return grid_f.eval()
