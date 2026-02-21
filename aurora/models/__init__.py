02b18ac4a0c153e74a62f7cab957ede1995e7849
Nazar Misyats <nazar.misyats@gmail.com>
2026-02-20 23:08:32 -0800

    Fixed remaining poly MLP import

--- aurora/models/__init__.py
+++ aurora/models/__init__.py
@@ -1,7 +1,6 @@
 from aurora.models.flux_model import FluxModel, ModelConfig
 from aurora.models.grid_flux import GridSampledFlux
 from aurora.models.spectral_mlp import SpectralMLP
-from aurora.models.poly_mlp import PolyMLP
 from aurora.models.hybrid_mlp import HybridMLP
 from aurora.models.residual_mlp import ResidualMLP
 from aurora.models.bilinear_mlp import BilinearMLP
@@ -15,7 +14,6 @@ __all__ = [
     "FluxModel",
     "GridSampledFlux",
     "SpectralMLP",
-    "PolyMLP",
     "HybridMLP",
     "ResidualMLP",
     "BilinearMLP",
