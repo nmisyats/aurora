289efc8cdd5aa954421e3764c85b49583d5b483a
Nazar Misyats <nazar.misyats@gmail.com>
2026-04-21 03:51:57 -0700

    Fixed scaling for flux plot

--- aurora/plot.py
+++ aurora/plot.py
@@ -156,7 +156,7 @@ def plot_flux_1d(
     
     if labels is not None:
         for data, label in zip(flux_data, labels):
-            ax.plot(energies, 10**3 * torch.pi * data, label=label, marker="x")
+            ax.plot(energies, 10**4 * torch.pi * data, label=label, marker="x")
         ax.legend()
     else:
         for data in flux_data:
